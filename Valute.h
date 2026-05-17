4c975702967fd9dd7372c9a9bcc2607dccde996c
tonikusturic <tonikusturic@gmail.com>
2026-05-17 22:14:28 +0200

    Update Valute.h

--- Valute.h
+++ Valute.h
@@ -1,23 +1,34 @@
-#pragma once
-#ifndef VALUTA_H
-#define VALUTA_H
-#define MAX_VALUTE 100
-#define MAX_VALUTE 100
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+/* (3,4) Struktura s jasnim nazivima */
 typedef struct {
-
-	char kod[4];
-	double tecaj;
+    char kod[4];          // kod valute, npr. "USD"
+    double tecaj;         // tečaj u odnosu na EUR
 } Valuta;
 
+/* (11) Enum izbornika */
 typedef enum {
+    IZBORNIK_DODAJ = 1,
+    IZBORNIK_PRIKAZI,
+    IZBORNIK_AZURIRAJ,
+    IZBORNIK_OBRISI,
+    IZBORNIK_SORTIRAJ,
+    IZBORNIK_TRAZI,
+    IZBORNIK_KONVERTIRAJ,
+    IZBORNIK_RESET,
+    IZBORNIK_IZLAZ
+} IzbornikOpcija;
 
-	DODAJ = 1, PRIKAZI, AZURIRAJ, IZLAZ
-}IzbornikOpcije;
+/* (8) extern globalna varijabla */
+extern int brojacDodavanja;
 
+/* Funkcije (10,13) */
 void dodajValutu();
 void prikaziValute();
 void azurirajValutu();
-#endif // !VALUTA_H
+void obrisiValutu();
+void sortirajValute();
+void traziValutu();
+void konvertirajValutu();

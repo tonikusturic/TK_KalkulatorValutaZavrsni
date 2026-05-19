6b70cb40b767846c1a73367a2054adde7115d529
tonikusturic <tonikusturic@gmail.com>
2026-05-19 10:13:05 +0200

    Update valute.c

--- valute.c
+++ valute.c
@@ -1,4 +1,5 @@
-#include "valuta.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "valute.h"
 #include "util.h"
 #include <string.h>
 #include <errno.h>
@@ -57,7 +58,6 @@ void prikaziValute() {
         lista[broj++] = temp;
     }
 
-    printf("Prosjecni tecaj: %.3lf\n", prosjecniTecaj(lista, broj));
 
     free(lista);
     fclose(dat);
@@ -86,9 +86,12 @@ void azurirajValutu() {
             printf("Azurirano.\n");
             return;
         }
+        else
+        {
+            printf("Valuta nije pronadena.\n");
+        }
     }
-
-    printf("Valuta nije pronadena.\n");
+   
     free(lista);
 }
 
@@ -113,24 +116,7 @@ void obrisiValutu() {
     printf("Valuta obrisana.\n");
 }
 
-/* ------------------ SORT ------------------ */
-void sortirajValute() {
-    Valuta* lista = NULL;
-    int broj = ucitajSveValute(&lista);
 
-    qsort(lista, broj, sizeof(Valuta), usporedbaTecajeva);
-
-    spremiSveValute(lista, broj);
-    free(lista);
-
-    printf("Sortirano po tecaju.\n");
-}
-
-int usporedbaTecajeva(const void* a, const void* b) {
-    const Valuta* v1 = (const Valuta*)a;
-    const Valuta* v2 = (const Valuta*)b;
-    return (v1->tecaj > v2->tecaj) - (v1->tecaj < v2->tecaj);
-}
 
 /* ------------------ SEARCH (rekurzija) ------------------ */
 int rekurzivnoTrazi(Valuta* lista, int broj, const char* kodValute) {
@@ -153,13 +139,11 @@ void traziValutu() {
 
     int indeks = rekurzivnoTrazi(lista, broj, trazeniKod);
 
-    if (indeks >= 0)
+    if (indeks > 0)
         printf("Pronadena %s (tecaj %.3lf)\n", lista[indeks].kod, lista[indeks].tecaj);
     else
         printf("Valuta nije pronadena.\n");
 
-    strcpy(zadnjiKod, trazeniKod);
-    printf("Zadnji trazeni kod: %s\n", zadnjiKod);
 
     free(lista);
 }
@@ -186,14 +170,67 @@ void konvertirajValutu() {
         if (strcmp(lista[i].kod, kodU) == 0) tecajU = lista[i].tecaj;
     }
 
-    if (tecajIz == -1 || tecajU == -1) {
+    if (tecajIz != -1 || tecajU != -1) {
+        double rezultat = iznos * (tecajU / tecajIz);
+        if (rezultat < 0) {
+            rezultat *= -1;
+        }
+        printf("%.2lf %s = %.2lf %s\n", iznos, kodIz, rezultat, kodU);
+
+        free(lista);
+    }
+    else {
         printf("Valuta ne postoji.\n");
         free(lista);
         return;
     }
+    
+}
 
-    double rezultat = iznos * (tecajU / tecajIz);
-    printf("%.2lf %s = %.2lf %s\n", iznos, kodIz, rezultat, kodU);
+/* ------------------ REMOVE ------------------ */
 
-    free(lista);
+void resetirajDatoteku() {
+
+    if (remove(PUTANJA_DATOTEKE) == 0) {
+        printf("Datoteka obrisana.\n");
+    }
+    else
+    {
+        perror("Brisanje nije uspjelo.\n");
+    }
+}
+/* ------------------ LOADING AND SAVING ------------------ */
+
+void spremiSveValute(Valuta* lista, int broj) {
+
+    FILE* dat = fopen(PUTANJA_DATOTEKE, "w");
+
+    if (!dat) {
+        perror("Greska upisa");
+        return;
+    }
+    for (int i = 0; i < broj; i++) {
+        fprintf(dat, "%s %.lf\n", lista[i].kod, lista[i].tecaj);
+    }
+    fclose(dat);
+
+
+}
+
+int ucitajSveValute(Valuta** lista) {
+
+    FILE* dat = fopen(PUTANJA_DATOTEKE, "r");
+    if (!dat) {
+        perror("Greska pri otvaranju");
+        return 0;
+    }
+    *lista = sigurnoAlociraj(sizeof(Valuta) * MAX_VALUTA);
+    int broj = 0;
+    while (fscanf(dat, "%3s %lf", (*lista)[broj].kod, &(*lista)[broj].tecaj) == 2)
+    {
+        broj++;
+        fclose(dat);
+        return broj;
+    }
+
 }
